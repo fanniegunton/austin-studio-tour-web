77e37fabc2ce778975831c8db49cdbb2d30e33ef
Fannie Gunton <fanniegunton@gmail.com>
2021-11-13 23:17:19 -0600

    Update dev tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,41 @@ module.exports = {
   parser: "babel-eslint",
   extends: [
     "standard",
+    "eslint:recommended",
+    "plugin:import/errors",
     "plugin:react/recommended",
     "plugin:prettier/recommended",
   ],
-  plugins: ["standard", "react", "react-hooks", "prettier"],
+  plugins: ["react-hooks"],
   rules: {
-    "react/prop-types": 1,
+    "prettier/prettier": "warn",
+    "react/display-name": "off",
+    "react/prop-types": "warn",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
   env: {
     jest: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+
+    // This configuration tells eslint-plugin-import to use
+    // the webpack resolver, and configures the webpack resolver
+    // to match the Gatsby webpack configuration. Our webpack
+    // config mostly comes from Gatsby, and we augment it in our
+    // gatsby-node.js file in the onCreateWebpackConfig function.
+    "import/resolver": {
+      webpack: {
+        config: {
+          resolve: {
+            modules: ["src", "node_modules"],
+            extensions: [".js", ".jsx"],
+          },
+        },
+      },
+    },
   },
 }
